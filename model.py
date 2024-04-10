688be246444be49ac64e13e96bf51aebc4c25370
Emily <emily.j.langley@gmail.com>
2024-04-10 13:53:11 -0700

    added __repr__ to Users() class and improved file readability

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
+"""Models for SteepSpots app."""
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+from datetime import datetime
+now = datetime.now()
 
 # link to data model:
 # https://dbdiagram.io/d/SteepSpots-660c79fb03593b6b6101cfe5
@@ -22,6 +27,7 @@ db = SQLAlchemy()
 #     salary = db.Column(db.Integer, nullable=True)
 
 #     dept = db.relationship('Department', back_populates="employees")
+#     variable = db.relationship('ClassName', back_populates='other variable name')
 
 #     def __repr__(self):
 #         return f"<Employee id={self.employee_id} name={self.name}>"
@@ -35,51 +41,123 @@ class Users(db.model):
 
     __tablename__ = 'users'
 
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    fname = db.Column(db.Varchar(30), nullable=False)
-    lname = db.Column(db.Varchar(30), nullable=False)
-    email = db.Column(db.Varchar(75), nullable=False)
-    password = db.Column(db.Varchar(30), nullable=False)
-    # start_date = db.Column(db.DateTime, default=`now()`)
+    user_id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
+        )
+    fname = db.Column(
+        db.Varchar(30),
+        nullable=False
+        )
+    lname = db.Column(
+        db.Varchar(30),
+        nullable=False
+        )
+    email = db.Column(
+        db.Varchar(75),
+        nullable=False,
+        unique=True
+        )
+    password = db.Column(
+        db.Varchar(30),
+        nullable=False
+        )
+    start_date = db.Column(
+        db.DateTime,
+        default=datetime.now()
+        )
 
-    favorite = db.relationship('Favorite', back_populates='userFavorites') # do I need this here? or just on the many side?
-    assoc_user_tea = db.relationship('AssocUserTea', back_populates='assocUserTeas')
+    favorites = db.relationship('Favorites', back_populates='user')
+    # assoc_user_tea = db.relationship('AssocUserTea', back_populates='assocUserTeas')
 
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
+    
 
-class UserFavorites(db.model):
+class Favorites(db.model):
     """User's favorite teas."""
 
     __tablename__ = 'userFavorites'
 
-    user_fav_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    tea_id = db.Column(db.Integer, db.ForeignKey('teas.tea_id'), nullable=False)
+    user_fav_id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.user_id'),
+        nullable=False
+        )
+    tea_id = db.Column(
+        db.Integer,
+        db.ForeignKey('teas.tea_id'),
+        nullable=False
+        )
     # date_favorited = db.Column(db.DateTime, default=`now()`)
 
-    user = db.relationship('User', back_populates='userFavorites')
+    user = db.relationship('Users', back_populates='favorites')
+    tea = db.relationship('Teas', back_populates='saved_by')
 
 
-class UserRatings(db.model):
+class Ratings(db.model):
     """User's ratings of teas."""
 
     __tablename__ = 'userRatings'
 
-    rating_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    tea_id = db.Column(db.Integer, db.ForeignKey('teas.tea_id'), nullable=False)
-    rating = db.Column(db.Integer, nullable=False)
+    rating_id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
+        )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.user_id'),
+        nullable=False
+        )
+    tea_id = db.Column(
+        db.Integer,
+        db.ForeignKey('teas.tea_id'),
+        nullable=False
+        )
+    rating = db.Column( # 1-5 rating
+        db.Integer,
+        nullable=False
+        )
     # date_rated = db.Column(db.DateTime, default=`now()`)
 
+    # tip: in the lecture slides, ratings are users and reviews are employees 
+    # in the one-to-one section
 
-class UserReviews(db.model):
+
+class Reviews(db.model):
     """User's reviews of teas."""
 
     __tablename__ = 'userReviews'
 
-    review_id = db.Column(db.Integer, db.ForeignKey('userRatings.rating_id'), primary_key=True, autoincrement=True, nullable=False) # am I doing this right if review_id and rating_id are one to one?
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    tea_id = db.Column(db.Integer, db.ForeignKey('teas.tea_id'), nullable=False)
-    review = db.Column(db.varchar(255), nullable=True)
+    review_id = db.Column(
+        db.Integer,
+        db.ForeignKey('userRatings.rating_id'),
+        primary_key=True,
+        nullable=False
+        )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.user_id'),
+        nullable=False
+        )
+    tea_id = db.Column(
+        db.Integer,
+        db.ForeignKey('teas.tea_id'),
+        nullable=False
+        )
+    review = db.Column(
+        db.varchar(255),
+        nullable=True
+        )
     # date_reviewed = db.Column(db.DateTime, default=`now()`)
 
 
@@ -88,17 +166,43 @@ class Teas(db.model):
 
     __tablename__ = 'teas'
 
-    tea_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    tea_group = db.Column(db.varchar, nullable=False) # green, black, oolong, etc
-    tea_brand = db.Column(db.varchar(75), nullable=False) # lipton, etc
-    brand_flavor = db.Column(db.varchar(75), nullable=False) # none, sleepytime, peace, etc
-    tea_name = db.Column(db.varchar(50), nullable=False) # milk tea with boba, iced tea with lemon
-    tea_class = db.Column(db.varchar, nullable=False) # herbal, decaf, caffeinated, etc
-    caff_range_mg = db.Column(db.varchar, nullable=False) # 90-120mg, 0-30 mg, etc
-    hot_cold = db.Column(db.varchar(4), nullable=False)
+    tea_id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True,
+        nullable=False
+        )
+    tea_group = db.Column( # green, black, oolong, etc
+        db.varchar,
+        nullable=False
+        )
+    tea_brand = db.Column( # lipton, etc
+        db.varchar(75),
+        nullable=False
+        )
+    brand_flavor = db.Column( # none, sleepytime, peace, etc
+        db.varchar(75),
+        nullable=False
+        )
+    tea_name = db.Column( # milk tea with boba, iced tea with lemon
+        db.varchar(50),
+        nullable=False
+        )
+    tea_class = db.Column( # herbal, decaf, caffeinated, etc
+        db.varchar,
+        nullable=False
+        )
+    caff_range_mg = db.Column( # 90-120mg, 0-30 mg, etc
+        db.varchar,
+        nullable=False
+        )
+    hot_cold = db.Column(
+        db.varchar(4),
+        nullable=False
+        )
     # date_added = db.Column(db.DateTime, default=`now()`)
 
-    assoc_user_tea = db.relationship('AssocUserTea', back_populates='assocUserTeas')
+    saved_by = db.relationship('Favorites', back_populates='tea')
 
 
 class FlavorProfiles(db.model):
@@ -117,17 +221,19 @@ class FoodPairings(db.model):
     pass
 
 
-class AssocUserTeas(db.model):
-    """Association table between Users and Teas."""
+# class AssocUserTeas(db.model):
+#     """Association table between Users and Teas."""
 
-    __tablename__ = 'assocUserTeas'
+#     __tablename__ = 'assocUserTeas'
 
-    user_tea_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
-    tea_id = db.Column(db.Integer, db.ForeignKey('teas.tea_id'), nullable=False)
+#     user_tea_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+#     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable=False)
+#     tea_id = db.Column(db.Integer, db.ForeignKey('teas.tea_id'), nullable=False)
 
-    user = db.relationship('User', back_populates='assocUserTeas')
-    tea = db.relationship('Tea', back_populates='assocUserTeas')
+#     user = db.relationship('User', back_populates='assocUserTeas')
+#     tea = db.relationship('Tea', back_populates='assocUserTeas')
+
+    # this table may be redundant, see userFavorites and decide
 
 
 class AssocTeaFlavors(db.model):
