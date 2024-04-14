e693b52e2ca192611e31ad449c4dbcfaa22f746e
Emily <emily.j.langley@gmail.com>
2024-04-14 15:23:43 -0700

    boiler plate draft

---
+++ crud.py
@@ -0,0 +1,23 @@
+from model import(
+    db,
+    <tables here>
+    )
+from werkzeug.security import generate_password_hash, check_password_hash
+
+################################################################################
+# TODO - add crud functions
+
+
+
+
+
+
+
+
+################################################################################
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
+    db.create_all()
