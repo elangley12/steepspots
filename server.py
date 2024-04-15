c5198b9fa22df0aa3d0eae9e5fe324f04b3cd8fb
Emily <emily.j.langley@gmail.com>
2024-04-15 16:14:25 -0700

    boiler plate update

--- server.py
+++ server.py
@@ -4,3 +4,12 @@ from flask import Flask, render_template, request, flash, session, redirect
 
 app = Flask(__name__)
 #   ... your routes, view functions, and everything else can come later ...
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", debug=True)
