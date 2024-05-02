05d4cebcfcd7e2311f61a09e94c71c58ad945d11
Emily <emily.j.langley@gmail.com>
2024-05-02 14:30:51 -0700

    corrected results being displayed on the userprofile.html

--- server.py
+++ server.py
@@ -113,7 +113,12 @@ def show_user_profile():
     user_id = session['user']
     user = crud.find_user_by_id(user_id)
     user_favorites = user.favorites
-    print(user_favorites)
+    # [<Favorite>, <Favorite>]
+    # favorite.tea => <Tea> => caffeine, tea_group
+    # favorite.user => <User>.favorites
+
+
+    # print(user_favorites)
 
     return render_template("userprofile.html", user_favorites=user_favorites)
 
