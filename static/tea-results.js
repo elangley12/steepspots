e43dababd4bf176c802f8345220d07ebadd099ba
Emily <emily.j.langley@gmail.com>
2024-05-08 17:41:05 -0700

    added more elements from model to cards on homepage and tested

--- static/tea-results.js
+++ static/tea-results.js
@@ -1,6 +1,9 @@
 'use strict';
 // here's a good spot to add an alert for testing connections
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 function showTeaResults(evt) {
     evt.preventDefault();
   
@@ -28,11 +31,20 @@ function showTeaResults(evt) {
           const result = 
             `<div class="col">
             <div class="card">
-              <img src="${tea_item.tea_img}" class="card-img-top tea-cards" alt="Image of tea">
-              <div class="card-body">
-                <h5 class="card-title">Tea Name: ${tea_item.tea_name}</h5>
-                <p class="card-text">Origin: ${tea_item.tea_origin}</p>
-                <a class="btn btn-outline-dark" href="/tea-profile/${tea_item.tea_id}">Spot Tea</a>
+            <img src="${tea_item.tea_img}" class="card-img-top tea-cards" 
+            data-bs-toggle="tooltip" data-bs-placement="top" 
+            data-bs-custom-class="tea-img-tooltip"
+            data-bs-title="${ tea_item.tea_flavor_notes }" 
+            alt="Image of tea">
+            <div class="card-body">
+              <h5 class="card-title">${ tea_item.tea_name } (${tea_item.tea_origin})</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${ tea_item.tea_group } tea</h6>
+              <h6 class="card-subtitle mb-2 text-muted">${ tea_item.caff_level } caffeine (${ tea_item.caff_range_mg } per cup)</h6>
+              <p class="card-text">${ tea_item.tea_info }</p>
+              <a class="btn btn-outline-dark" data-bs-toggle="tooltip" 
+              data-bs-placement="bottom" data-bs-custom-class="spot-tea-tooltip"
+              data-bs-title="click to save favorite" 
+              href="/tea-profile/${ tea_item.tea_id }">Spot Tea</a>
               </div>
             </div>
           </div>`;
